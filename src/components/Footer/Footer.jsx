462ea2dac4139da90cbe4b87f51e23f10bc3a40d
triggerdeva <120394539+triggerdeva@users.noreply.github.com>
2024-02-01 15:38:28 +0530

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Wagholi, Pune</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+91 9304118781</p>
                 </div>
               </div>
             </div>
